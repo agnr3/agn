7878ef7172d456b2a5fc326bed62d9781b502d8f
agnr3 <102449278+agnr3@users.noreply.github.com>
2022-03-28 21:07:39 -0300

    Delete js directory

--- js/ubicacion.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("btn_open").addEventListener("click", open_close_menu);
-var side_menu = document.getElementById('menu_side');
-var btn_open = document.getElementById('btn_open');
-
-function open_close_menu(){
-  menu_side.classList.toggle("menu__side_move");
-}
