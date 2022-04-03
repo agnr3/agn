9c944fa881fd0391adc70eb83fbe674d03e0be5e
agnr3 <102449278+agnr3@users.noreply.github.com>
2022-04-03 02:08:49 -0300

    Add files via upload

---
+++ js/precios.js
@@ -0,0 +1,41 @@
+var articulo1 = document.getElementById("shop__article");
+var afectado1 = document.getElementById("shop__picture");
+articulo1.addEventListener("mouseover", () => {
+  afectado1.className =  "shop__pictures afectado";
+}, false)
+articulo1.addEventListener("mouseout", () => {
+  afectado1.className = "shop__pictures";
+}, false)
+
+
+
+var articulo2 = document.getElementById("shop__article2");
+var afectado2 = document.getElementById("shop__picture2");
+articulo2.addEventListener("mouseover", () => {
+  afectado2.className =  "shop__pictures afectado";
+}, false)
+articulo2.addEventListener("mouseout", () => {
+  afectado2.className = "shop__pictures";
+}, false)
+
+
+
+var articulo3 = document.getElementById("shop__article3");
+var afectado3 = document.getElementById("shop__picture3");
+articulo3.addEventListener("mouseover", () => {
+  afectado3.className =  "shop__pictures afectado";
+}, false)
+articulo3.addEventListener("mouseout", () => {
+  afectado3.className = "shop__pictures";
+}, false)
+
+
+
+var articulo4 = document.getElementById("shop__article4");
+var afectado4 = document.getElementById("shop__picture4");
+articulo4.addEventListener("mouseover", () => {
+  afectado4.className =  "shop__pictures afectado";
+}, false)
+articulo4.addEventListener("mouseout", () => {
+  afectado4.className = "shop__pictures";
+}, false)
