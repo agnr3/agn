eb61aa4baa06967d845adcc86309b5d1ba8edf6a
agnr3 <102449278+agnr3@users.noreply.github.com>
2022-04-17 20:05:51 -0300

    Update nav_responsive.js

--- js/nav_responsive.js
+++ js/nav_responsive.js
@@ -1,12 +1,1 @@
-var menu = document.getElementById("menu__responsive1");
-var contenedor = document.getElementById("nav__menuresponsivo1");
-var close = document.getElementById("menu__responsive2");
-
-
-menu.addEventListener("click" , () => {
-  nav__menuresponsivo1.className = "nav__menuresponsivo active";
-});
-
-close.addEventListener("click" , () => {
-  nav__menuresponsivo1.className = "nav__menuresponsivo";
-});
+var menu=document.getElementById("menu__responsive1");var contenedor=document.getElementById("nav__menuresponsivo1");var close=document.getElementById("menu__responsive2");menu.addEventListener("click" , ()=>{nav__menuresponsivo1.className="nav__menuresponsivo active";});close.addEventListener("click" , ()=>{nav__menuresponsivo1.className="nav__menuresponsivo";});
